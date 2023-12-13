d979ffbdacff63d8e366bd394f42e95017f1ecfe
Tetahio Ruihau <ruihau-emmanuel.tetahio@inetum.com>
2023-12-13 11:01:44 +0100

    prerelease

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,9 @@ async function run() {
         } else {
             core.startGroup('Create new stack');
             core.info("Creating new stack...");
+            core.info(`Stack name: ${cfg.stack.name}`);
+            core.info(`Stack endpoint: ${cfg.portainer.endpoint}`);
+            core.info(`Stack file: ${cfg.stack.file}`);
             await portainer.createStack({
                 endpoint: cfg.portainer.endpoint,
                 name: cfg.stack.name,
